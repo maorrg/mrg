0514f19844490da4a5b6fca123c4a7639bbfe9ed
Maor Roizman Gheiler <maor.roizman@utec.edu.pe>
2023-01-03 00:47:38 +0100

    Final changes

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,9 @@ import {
   Container,
   Heading,
   Box,
-  SimpleGrid,
   Button,
   List,
   ListItem,
-  useColorModeValue,
   chakra
 } from '@chakra-ui/react'
 import { ChevronRightIcon } from '@chakra-ui/icons'
@@ -16,25 +14,22 @@ import Paragraph from '../components/paragraph'
 import { BioSection, BioYear } from '../components/bio'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
-import { GridItem } from '../components/grid-item'
-import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
+import { IoLogoGithub } from 'react-icons/io5'
 import { FaLinkedin } from 'react-icons/fa'
-import thumbYouTube from '../public/images/links/youtube.png'
-import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
 import Image from 'next/image'
 
 const ProfileImage = chakra(Image, {
-  shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
+  shouldForwardProp: (prop) => ['width', 'height', 'src', 'alt'].includes(prop)
 })
 
 const Home = () => (
   <Layout>
     <Container>
       <Box
-        borderRadius="lg"
+        borderRadius='lg'
         mb={6}
         p={3}
-        textAlign="center"
+        textAlign='center'
         // bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
         // css={{ backdropFilter: 'blur(10px)' }}
       >
@@ -43,7 +38,7 @@ const Home = () => (
 
       <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
+          <Heading as='h2' variant='page-title'>
             Maor Roizman
           </Heading>
           <p>Software Developer</p>
@@ -52,17 +47,17 @@ const Home = () => (
           flexShrink={0}
           mt={{ base: 4, md: 0 }}
           ml={{ md: 6 }}
-          textAlign="center"
+          textAlign='center'
         >
           <Box
-            borderColor="whiteAlpha.800"
+            borderColor='whiteAlpha.800'
             borderWidth={2}
-            borderStyle="solid"
-            w="100px"
-            h="100px"
-            display="inline-block"
-            borderRadius="full"
-            overflow="hidden"
+            borderStyle='solid'
+            w='100px'
+            h='100px'
+            display='inline-block'
+            borderRadius='full'
+            overflow='hidden'
           >
             {/* <ProfileImage
               src="/images/mrg.jpeg"
@@ -76,7 +71,7 @@ const Home = () => (
       </Box>
 
       <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
+        <Heading as='h3' variant='section-title'>
           Work
         </Heading>
         <Paragraph>
@@ -86,19 +81,19 @@ const Home = () => (
           way to solving real-life problems with code. When not online, he loves
           hanging out with his camera. Currently, he is living off of his own
           product called{' '}
-          <NextLink href="/works/inkdrop" passHref scroll={false}>
+          <NextLink href='/works/inkdrop' passHref scroll={false}>
             <Link>Inkdrop</Link>
           </NextLink>
           . He publishes content for marketing his products and his YouTube
           channel called &quot;
-          <NextLink href="https://www.youtube.com/devaslife" passHref>
-            <Link target="_blank">Dev as Life</Link>
+          <NextLink href='https://www.youtube.com/devaslife' passHref>
+            <Link target='_blank'>Dev as Life</Link>
           </NextLink>
           &quot; has more than 100k subscribers.
         </Paragraph>
-        <Box align="center" my={4}>
-          <NextLink href="/works" passHref scroll={false}>
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+        <Box align='center' my={4}>
+          <NextLink href='/works' passHref scroll={false}>
+            <Button rightIcon={<ChevronRightIcon />} colorScheme='teal'>
               My portfolio
             </Button>
           </NextLink>
@@ -106,7 +101,7 @@ const Home = () => (
       </Section>
 
       <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
+        <Heading as='h3' variant='section-title'>
           Bio
         </Heading>
         <BioSection>
@@ -125,16 +120,16 @@ const Home = () => (
       </Section>
 
       <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
+        <Heading as='h3' variant='section-title'>
           I ♥
         </Heading>
         <Paragraph>
           Art, Music,{' '}
-          <Link href="https://illust.odoruinu.net/" target="_blank">
+          <Link href='https://illust.odoruinu.net/' target='_blank'>
             Drawing
           </Link>
           , Playing Drums,{' '}
-          <Link href="https://500px.com/p/craftzdog" target="_blank">
+          <Link href='https://500px.com/p/craftzdog' target='_blank'>
             Photography
           </Link>
           , Leica, Machine Learning
@@ -142,15 +137,15 @@ const Home = () => (
       </Section>
 
       <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
+        <Heading as='h3' variant='section-title'>
           On the web
         </Heading>
         <List>
           <ListItem>
-            <Link href="https://github.com/maorrg" target="_blank">
+            <Link href='https://github.com/maorrg' target='_blank'>
               <Button
-                variant="ghost"
-                colorScheme="teal"
+                variant='ghost'
+                colorScheme='teal'
                 leftIcon={<IoLogoGithub />}
               >
                 @maorrg
@@ -158,10 +153,10 @@ const Home = () => (
             </Link>
           </ListItem>
           <ListItem>
-            <Link href="https://www.linkedin.com/in/maorrg/" target="_blank">
+            <Link href='https://www.linkedin.com/in/maorrg/' target='_blank'>
               <Button
-                variant="ghost"
-                colorScheme="teal"
+                variant='ghost'
+                colorScheme='teal'
                 leftIcon={<FaLinkedin />}
               >
                 @maorrg
@@ -187,9 +182,9 @@ const Home = () => (
           </GridItem>
         </SimpleGrid> */}
 
-        <Box align="center" my={4}>
-          <NextLink href="/posts" passHref scroll={false}>
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+        <Box align='center' my={4}>
+          <NextLink href='/posts' passHref scroll={false}>
+            <Button rightIcon={<ChevronRightIcon />} colorScheme='teal'>
               Popular posts
             </Button>
           </NextLink>
